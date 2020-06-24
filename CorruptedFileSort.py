b03493b3449c4d7a6bc315a4bda8ed212374497b
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-06-24 13:57:38 +0200

    Added a python script to prune corrupted videos from the dataset. Found 17 corrupted videos out of 6k videos in the training set

---
+++ CorruptedFileSort.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+import os
+import shutil as sh
+
+dataset_video_path = 'E:/Programmation/Python/PAF 2020/first-order-model/data/vox-png/train/'
+corrupted_dump = 'E:/Programmation/Python/PAF 2020/first-order-model/data/vox-png/corrupted/'
+
+def isFileCorrupted(path):
+
+    return (os.stat(path).st_size <= 30) # If file size is inferior to 30 bytes, return true
+
+folders = os.listdir(dataset_video_path)
+n = len(folders)
+corrupted_number = 0
+for i, f in enumerate(folders):
+    files = os.listdir(dataset_video_path + f)
+    corrupted = False
+    # Basically: If one image is corrupted, move the complete video out of the folder to the corrupted dump
+    for file in files:
+        if (isFileCorrupted(dataset_video_path + f + "/" + file)):
+            corrupted = True
+    if corrupted:
+        sh.move(dataset_video_path + f , corrupted_dump + f)
+        corrupted_number += 1
+
+
+    if i % 100 == 99:
+        print("Videos : " + str(i) + " / " + str(n))
+
+print("Corrupted files pruning finished.")
+print("Found [" + str(corrupted_number) + "] corrupted videos. ")
---
+++ Deep_fake_generation_Interruption_handler_for_better_distributed_dataset_in_fake_class.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import os
+from PIL import Image
+import shutil as sh
+import numpy as np
+
+
+"""
+This script is used in order to remove from the dataset the videos that have already been
+used for deepfake generation by moving them to another designated folder. Thus,
+once all the video are used, it's possible to continue generating with first-order model
+without re-using a video. This leads to a better distribution for the fake class in the final dataset
+"""
+
+"""
+File name structure:
+-------------Driving-------------      --------------Source-------------
+id10001#DtdEYdViWdw#001613#001802.mp4-id10903#MsJKfBcIHrI#005322#005464.mp4
+"""
+
+fake_png_path = 'E:/Programmation/Python/PAF 2020/first-order-model/checkpoint/animation/png/'
+dataset_video_path = 'E:/Programmation/Python/PAF 2020/first-order-model/data/vox-png/train/'
+move_to_path = 'E:/Programmation/Python/PAF 2020/first-order-model/data/vox-png/used_for_fake-train/'
+
+files = os.listdir(fake_png_path)
+n = len(files)
+for i, f in enumerate(files):
+
+    f1 = f[:37]
+    f2 = f[38:-4]
+    if os.path.exists(dataset_video_path + f1):
+        sh.move(dataset_video_path + f1, move_to_path + f1)
+
+    if os.path.exists(dataset_video_path + f2):
+        sh.move(dataset_video_path + f2, move_to_path + f2)
+
+    print("Progress: " + str(i) + " / " + str(n))
+
+
+
+
+
+
+
+
+
+
+
